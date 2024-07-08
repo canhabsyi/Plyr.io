16cbb772f8665570780bf57278e7619dfa0dbb95
Canhabsyi <67394957+canhabsyi@users.noreply.github.com>
2024-07-08 18:35:34 +0900

    Update player.js

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 // Change "{}" to your options:
 // https://github.com/sampotts/plyr/#options
-const player = new Plyr('#player', {autoplay: true, muted: true, volume: 0});
+const player = new Plyr('#player', {autoplay: true, muted: false, volume: 0});
 
 // Expose player so it can be used from the console
 window.player = player;
